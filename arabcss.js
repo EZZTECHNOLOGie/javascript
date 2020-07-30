e4465cfd743848cbd448f26c048f8f7e736ef59c
EZZTECHNOLOGie <68975661+EZZTECHNOLOGie@users.noreply.github.com>
2020-07-30 02:54:37 +0200

    Update and rename arabcss.css to arabcss.js

---
+++ arabcss.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+//<![CDATA[
+//CSS Ready
+
+//CSS Ready
+function loadCSS(e,t,n){"use strict";var i=window.document.createElement("link");var o=t||window.document.getElementsByTagName("script")[0];i.rel="stylesheet";i.href=e;i.media="only x";o.parentNode.insertBefore(i,o);setTimeout(function(){i.media=n||"all"})}
+loadCSS("//fonts.googleapis.com/css?family=Ruda:400,700,900|Open+Sans:400,600,700,400italic|Black+Ops+One");loadCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
+//]]>
+</script>
